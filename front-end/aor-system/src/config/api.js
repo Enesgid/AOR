f896a71d75b9bdc97071f0b5f54416550f55e5ba
enesgid <agenyigi@gmail.com>
2026-09-25 14:53:27 +0100

    api update

--- front-end/aor-system/src/config/api.js
+++ front-end/aor-system/src/config/api.js
@@ -1,4 +1,6 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "https://aor-q19z.onrender.com";
+  import.meta.env.VITE_API_URL !== undefined
+    ? import.meta.env.VITE_API_URL.trim()
+    : "https://aor-q19z.onrender.com";
 
 export default API_BASE_URL;
